7e25054d5f9e2eae7f14f11db983a3d1d70c1dc4
aksharshah63 <85052254+chavdarajesh@users.noreply.github.com>
2023-05-28 22:12:12 +0530

    sw firdt get network

--- sw.js
+++ sw.js
@@ -104,17 +104,16 @@ self.addEventListener('install', function (event) {
 
 // Listen for request events
 self.addEventListener('fetch', function (event) {
-
     // Get the request
     let request = event.request;
-
+    
     // Bug fix
     // https://stackoverflow.com/a/49719964
     if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
-
+    
     // HTML files
     // Network-first
-    if (request.headers.get('Accept').includes('text/html')) {
+    // if (request.headers.get('Accept').includes('text/html','text')) {
         event.respondWith(
             fetch(request).then(function (response) {
 
@@ -136,43 +135,43 @@ self.addEventListener('fetch', function (event) {
 
             })
         );
-    }
+    // }
 
     // CSS & JavaScript
     // Offline-first
-    if (request.headers.get('Accept').includes('text/css') || request.headers.get('Accept').includes('text/javascript')) {
-        event.respondWith(
-            caches.match(request).then(function (response) {
-                return response || fetch(request).then(function (response) {
+    // if (request.headers.get('Accept').includes('text/css') || (request.headers.get('Accept').includes('text/javascript') && request.headers.get('Accept').includes('*/*') ) || request.headers.get('Accept').includes('*/*') ) {
+    //     event.respondWith(
+    //         caches.match(request).then(function (response) {
+    //             return response || fetch(request).then(function (response) {
 
-                    // Return the response
-                    return response;
+    //                 // Return the response
+    //                 return response;
 
-                });
-            })
-        );
-        return;
-    }
+    //             });
+    //         })
+    //     );
+    //     return;
+    // }
 
     // Images
     // Offline-first
-    if (request.headers.get('Accept').includes('image')) {
-        event.respondWith(
-            caches.match(request).then(function (response) {
-                return response || fetch(request).then(async function (response) {
+    // if (request.headers.get('Accept').includes('image/*')) {
+    //     event.respondWith(
+    //         caches.match(request).then(function (response) {
+    //             return response || fetch(request).then(async function (response) {
 
-                    // Save a copy of it in cache
-                    let copy = response.clone();
-                    await event.waitUntil(caches.open('app').then(function (cache) {
-                        return cache.put(request, copy);
-                    }));
+    //                 // Save a copy of it in cache
+    //                 let copy = response.clone();
+    //                 await event.waitUntil(caches.open('app').then(function (cache) {
+    //                     return cache.put(request, copy);
+    //                 }));
 
-                    // Return the response
-                    return response;
+    //                 // Return the response
+    //                 return response;
 
-                });
-            })
-        );
-    }
+    //             });
+    //         })
+    //     );
+    // }
 
 });
